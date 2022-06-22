78091c40c6258e4a7e4e972d730991f6bb157bd5
rexim <reximkut@gmail.com>
2022-06-23 04:09:25 +0700

    foo -> wasmstub

---
+++ wasmstub.c
@@ -0,0 +1,8 @@
+// This function is refered by main.jai. The signature here does not matter.
+// What matters is the symbol name in the generated libwasmstub.so dynamic library.
+//
+// The idea is to trick the Jai compiler into treating this as an external function,
+// so it can be substituded in load.js.
+void bar(void)
+{
+}
