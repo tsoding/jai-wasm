3783ba72372cc21fba2998001e4ef760103ff180
rexim <reximkut@gmail.com>
2022-06-23 20:19:23 +0700

    [load.js] autogenerated "NOT IMPLEMENTED" stubs for environment

--- load.js
+++ load.js
@@ -7,34 +7,24 @@ function find_name_by_prefix(exports, prefix) {
     return null;
 }
 
+function make_environment(env) {
+    return new Proxy(env, {
+        get(target, prop, receiver) {
+            if (!env.hasOwnProperty(prop)) {
+                return (...args) => {console.error("NOT IMPLEMENTED: "+prop, args)}
+            }
+            return env[prop];
+        }
+    });
+}
+
 WebAssembly.instantiateStreaming(fetch('./main_fixed.wasm'), {
-    // TODO: is it possible to come up with magical object that acts
-    // like a table and for any unknown names returns the function that
-    // prints the "NOT IMPLEMENTED" message?
-    //
-    // Maybe via Proxy: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
-    // 
-    // This would be useful in case the set of functions that require such stub
-    // changes between the version of the compiler.
-    "env": {
-        "memset": (...args) => {console.error("NOT IMPLEMENTED: memset", args)},
-        "malloc": (...args) => {console.error("NOT IMPLEMENTED: malloc", args)},
-        "realloc": (...args) => {console.error("NOT IMPLEMENTED: realloc", args)},
-        "free": (...args) => {console.error("NOT IMPLEMENTED: free", args)},
-        "memcpy": (...args) => {console.error("NOT IMPLEMENTED: memcpy", args)},
-        "stbsp_sprintf": (...args) => {console.error("NOT IMPLEMENTED: stbsp_sprintf", args)},
-        "memcmp": (...args) => {console.error("NOT IMPLEMENTED: memcmp", args)},
-        "write": (...args) => {console.error("NOT IMPLEMENTED: write", args)},
-        "pthread_mutexattr_init": (...args) => {console.error("NOT IMPLEMENTED: pthread_mutexattr_init", args)},
-        "pthread_mutexattr_settype": (...args) => {console.error("NOT IMPLEMENTED: pthread_mutexattr_settype", args)},
-        "pthread_mutex_init": (...args) => {console.error("NOT IMPLEMENTED: pthread_mutex_init", args)},
-        "pthread_mutex_lock": (...args) => {console.error("NOT IMPLEMENTED: pthread_mutex_lock", args)},
-        "pthread_mutex_unlock": (...args) => {console.error("NOT IMPLEMENTED: pthread_mutex_unlock", args)},
+    "env": make_environment({
         "bar": (a, b) => {
             console.log("YEP! Jai just called to JavaScript! It's a JaiScript now! LULW");
             return a + b + 1000000n;
         }
-    }
+    })
 }).then(wasmModule => {
     const sum = find_name_by_prefix(wasmModule.instance.exports, "sum_");
     sum(34n, 35n, 0);
